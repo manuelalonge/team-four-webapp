5f7c2c0579a9ee791dcdc1fe1b77e85c827ef97b
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-12-19 01:01:37 +0100

    images load function and 10 pictures limit added

---
+++ src/script.js
@@ -0,0 +1,29 @@
+const inputImage = document.querySelector("#images-upload");
+const imagesContainer = document.querySelector(".load-images-gallery__images-container");
+
+
+(function init() {
+    inputImage.addEventListener("change", selectImage);
+})();
+
+
+function selectImage() {
+    const image = this.files[0];
+    if(image) {
+        const reader = new FileReader();
+        reader.addEventListener("load", loadImage);
+        reader.readAsDataURL(image);
+    }
+    console.log(imagesContainer.children.length);
+}
+        
+function loadImage() {
+    if(imagesContainer.children.length < 10) {
+        const newImage = new Image();
+        newImage.src = this.result;
+        imagesContainer.appendChild(newImage);
+    } else {
+        console.log("error");
+    }
+    
+}
