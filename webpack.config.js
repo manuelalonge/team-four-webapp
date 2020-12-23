46d38119eb59130c636492cd6c6ea104b94bf097
LucaBert89 <62511269+LucaBert89@users.noreply.github.com>
2020-12-24 00:08:27 +0100

    webpack images problem fixed

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,21 @@ module.exports = {
                 }
             ],
             },
+            {
+              test: /\.html$/i,
+              loader: 'html-loader',
+              options: {
+                attributes: {
+                  list: [
+                    {
+                      tag: 'img',
+                      attribute: 'src',
+                      type: 'src',
+                    },
+                  ]
+                }
+              }
+            },
             { 
                 // fonts loader
                 test: /\.(woff|woff2|eot|ttf)$/,
@@ -41,11 +56,24 @@ module.exports = {
             // video
           // images asset/resouce: take all the images and put them to destination folder images
           { 
-            test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+            test: /\.(webp)$/i,
             type:"asset/resource",
             generator: {
               filename: 'images/[name].[ext]'
             }
+          },
+          { test: /\.(png|svg|jpg|gif|webm|mp4)$/,
+            use: [
+            {
+              loader: 'file-loader',
+              options: {
+                esModule: false,
+                name: "[name].[ext]",
+                outputPath: "images/",
+                publicPath: "images/",
+              } 
+            }
+            ]
           },
         ]
     },
