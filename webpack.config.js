96b9fda085cf4dc03a761318e71dea55137bcc6d
Manuel Alonge <manuel.alonge@gmail.com>
2021-01-15 08:58:29 +0100

    [FIX] scaffolding took back to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,16 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  devtool: 'eval-source-map',
-  entry: './flaskr/static/js/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, './flaskr/static/dist'),
-    assetModuleFilename: './flaskr/static/images/[name].[ext]'
+    path: path.resolve(__dirname, 'dist'),
+   assetModuleFilename: 'images/[name].[ext]'
   },
     devServer: {
-        contentBase: './flaskr/static/dist',
+        contentBase: './dist',
       },
     module: {
+
         rules: [
             {
             // scss loader
@@ -23,7 +22,7 @@ module.exports = {
                 {
                     loader: MiniCssExtractPlugin.loader,
                     options : {
-                        publicPath:"./flaskr/static/js"
+                        publicPath:"./"
                     },
                 },
                 {
@@ -34,64 +33,26 @@ module.exports = {
                 }
             ],
             },
-            {
-              test: /\.js$/,
-              exclude: /node_modules/,
-              use: ["babel-loader"]
-            },
-            {
-              test: /\.html$/i,
-              loader: 'html-loader',
-              options: {
-                attributes: {
-                  list: [
-                    {
-                      tag: 'img',
-                      attribute: 'src',
-                      type: 'src',
-                    },
-                  ]
-                }
-              }
-            },
             { 
                 // fonts loader
                 test: /\.(woff|woff2|eot|ttf)$/,
-                type: 'asset/resource'
+                type: 'asset/resource',
               },
             // video
           // images asset/resouce: take all the images and put them to destination folder images
           { 
-            test: /\.(webp)$/i,
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
             type:"asset/resource",
             generator: {
               filename: 'images/[name].[ext]'
             }
           },
-          { test: /\.(png|svg|jpg|gif|webm|mp4)$/,
-            use: [
-            {
-              loader: 'file-loader',
-              options: {
-                esModule: false,
-                name: "[name].[ext]",
-                outputPath: "images/",
-                publicPath: "images/",
-              } 
-            }
-            ]
-          },
         ]
     },
     plugins: [
-          /*new HtmlWebpackPlugin({
-            filename: "index.html", 
-            template: path.resolve(__dirname, "flaskr","templates","auth", "index.html")
-          }),*/
-          new HtmlWebpackPlugin({
-            filename: "landing-page.html_1",
-            template: path.resolve(__dirname, "flaskr","templates","upload", "landing_page_1.html")
-          }),
+        new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html")
+        }),
         new MiniCssExtractPlugin({
             filename: "style.css"
           }),
