e40a5569bd2f71eae2e8dde159f74b03f70bab81
Ion Vajda <v.ionut24@gmail.com>
2021-02-10 23:16:45 +0100

    issue resolved

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
-import './styles.scss';
+import "./styles.scss";
 
 // Login JS
 
-   /* The function below activates a toggle for the input field to use for password recovery (you insert there your e-mail and recover your password)
+/* The function below activates a toggle for the input field to use for password recovery (you insert there your e-mail and recover your password)
   By clicking on the link, the field is shown or hidden */
 
-  function toggleInputField() {
-    var input = document.querySelector('.login-section__psw-recovery-field');
-    input.classList.toggle('login-section__psw-recovery--display-block');       
-  }
-  
-  var link = document.querySelector('.login-section__psw-recovery-link');
-    
-  link.addEventListener('click', toggleInputField);
+function toggleInputField() {
+  var input = document.querySelector(".login-section__psw-recovery-field");
+  input.classList.toggle("login-section__psw-recovery--display-block");
+}
+
+var link = document.querySelector(".login-section__psw-recovery-link");
+
+link.addEventListener("click", toggleInputField);
+
+// Show error message functionality
+const form = document.querySelector(".login-form");
+const username = document.getElementById("username");
+const password = document.getElementById("password");
+const email = document.getElementById("email");
+
+// Event listeners
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,129 +1,127 @@
-const path = require('path');
-const buildPath = path.resolve(__dirname, 'dist');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const path = require("path");
+const buildPath = path.resolve(__dirname, "dist");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
-    index: './src/index.js',
-    registration:'./src/index.js',
-    landing_page: './src/landing_page.js',
+    index: "./src/index.js",
+    registration: "./src/registration.js",
+    landing_page: "./src/landing_page.js",
   },
   // https://webpack.js.org/concepts/output/
   output: {
-    filename: '[name].[hash:20].js',
-    path: buildPath
+    filename: "[name].[hash:20].js",
+    path: buildPath,
   },
 
   // https://webpack.js.org/configuration/dev-server/
   devServer: {
-    port: 8080
+    port: 8080,
   },
 
   module: {
     rules: [
-        {
+      {
         // scss loader
         test: /\.scss$/,
         use: [
-            // MINICSSPLUGIN: generate a style.css file inside dist folder with better performance (not through js). I added the public path because of an error
-            {
-                loader: MiniCssExtractPlugin.loader,
-                options : {
-                    publicPath:'./src'
-                },
+          // MINICSSPLUGIN: generate a style.css file inside dist folder with better performance (not through js). I added the public path because of an error
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "./src",
             },
-            {
-                loader: 'css-loader',
-            },
-            {
-                loader: 'sass-loader',
-            }
-        ],
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: ['babel-loader']
-        },
-        {
-          test: /\.html$/i,
-          loader: 'html-loader',
-          options: {
-            attributes: {
-              list: [
-                {
-                  tag: 'img',
-                  attribute: 'src',
-                  type: 'src',
-                },
-              ]
-            }
-          }
-        },
-        { 
-            // fonts loader
-            test: /\.(woff|woff2|eot|ttf)$/,
-            type: 'asset/resource'
           },
-        // video
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          attributes: {
+            list: [
+              {
+                tag: "img",
+                attribute: "src",
+                type: "src",
+              },
+            ],
+          },
+        },
+      },
+      {
+        // fonts loader
+        test: /\.(woff|woff2|eot|ttf)$/,
+        type: "asset/resource",
+      },
+      // video
       // images asset/resouce: take all the images and put them to destination folder images
-      { 
+      {
         test: /\.(webp)$/i,
-        type:'asset/resource',
+        type: "asset/resource",
         generator: {
-          filename: 'images/[name].[ext]'
-        }
+          filename: "images/[name].[ext]",
+        },
       },
-      { test: /\.(png|svg|jpg|gif|webm|mp4)$/,
+      {
+        test: /\.(png|svg|jpg|gif|webm|mp4)$/,
         use: [
-        {
-          loader: 'file-loader',
-          options: {
-            esModule: false,
-            name: '[name].[ext]',
-            outputPath: 'images/',
-            publicPath: 'images/',
-          } 
-        }
-        ]
+          {
+            loader: "file-loader",
+            options: {
+              esModule: false,
+              name: "[name].[ext]",
+              outputPath: "images/",
+              publicPath: "images/",
+            },
+          },
+        ],
       },
-    ]
+    ],
   },
 
   // https://webpack.js.org/concepts/plugins/
   plugins: [
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        inject: true,
-        chunks: ['index'],
-        filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/registration.html',
+      template: "./src/index.html",
       inject: true,
-      chunks: ['registration'],
-      filename: 'registration.html'
+      chunks: ["index"],
+      filename: "index.html",
     }),
     new HtmlWebpackPlugin({
-        template: './src/landing_page.html',
-        inject: true,
-        chunks: ['landing_page'],
-        filename: 'landing_page.html'
+      template: "./src/registration.html",
+      inject: true,
+      chunks: ["registration"],
+      filename: "registration.html",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./src/landing_page.html",
+      inject: true,
+      chunks: ["landing_page"],
+      filename: "landing_page.html",
     }),
     new MiniCssExtractPlugin({
       filename: "[name].[contenthash].css",
-      chunkFilename: "[id].[contenthash].css"
-    })
+      chunkFilename: "[id].[contenthash].css",
+    }),
   ],
   // https://webpack.js.org/configuration/optimization/
   optimization: {
     minimize: true,
-    minimizer: [
-        new OptimizeCssAssetsPlugin({})
-    ]
-}
+    minimizer: [new OptimizeCssAssetsPlugin({})],
+  },
 };
